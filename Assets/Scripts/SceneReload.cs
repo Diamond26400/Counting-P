d38bd63cceffab5ccfeb65a2e159dd2ad6456ac5
Diamond26400 <obaloluwajames5@gmail.com>
2023-12-20 06:26:26 +0100

    Update

--- Assets/Scripts/SceneReload.cs
+++ Assets/Scripts/SceneReload.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class Reload: MonoBehaviour
 
+public class SceneReload : MonoBehaviour
 {
-
-    private void Start()
+    // Start is called before the first frame update
+    void Start()
     {
         
     }
@@ -14,10 +14,6 @@ public class Reload: MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    public void Restart()
-    {
-        // SceneManager.LoadScene(SceneManager.GetActiveScene().BuildIndex);
+       
     }
 }
