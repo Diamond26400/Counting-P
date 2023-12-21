425947b5252c5cd9f3cebe5b2ff03312dbb4ee02
Diamond26400 <obaloluwajames5@gmail.com>
2023-12-21 22:40:49 +0100

    Update Healthmanager.cs

--- Assets/Healthmanager.cs
+++ Assets/Healthmanager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Healthmanager : MonoBehaviour
 {
+    public  Image healthBar;
+    public float HealthAmount = 100f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,17 @@ public class Healthmanager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void Takedamage(float damage)
+    {
+        HealthAmount -= damage;
+        healthBar.fillAmount = HealthAmount / 100f;
+    }
+    public void Heal(float healingAmount)
+    {
+        HealthAmount += healingAmount;
+        HealthAmount = Mathf.Clamp(HealthAmount, 0, 100);
+
+        healthBar.fillAmount= HealthAmount / 100f;
     }
 }
