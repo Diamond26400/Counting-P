1f41fa61254fd175a6f26e82b61f8a953dfa4abd
Diamond26400 <obaloluwajames5@gmail.com>
2023-12-23 17:08:10 +0100

    Update

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -7,23 +7,20 @@ public class GameManager : MonoBehaviour
 {
     private bool isGamePaused = false;
 
-
-   
     [SerializeField] GameObject canvas2;
     [SerializeField] GameObject canvas3;
 
     public string sceneToLoad = "Scene1";
 
-
     private void Start()
     {
-       // GameObject canvas1 = GameObject.Find("Canvas 1");
+        // GameObject canvas1 = GameObject.Find("Canvas 1");
         GameObject canvas2 = GameObject.Find("Canvas 2");
         GameObject canvas3 = GameObject.Find("Canvas 3");
 
-        //LoadScene();
+       
     }
-    void Update()
+    void FixedUpdate()
     {
         // Check for input or events to trigger the different functionalities
 
@@ -31,38 +28,35 @@ public class GameManager : MonoBehaviour
         {
             if (isGamePaused)
             {
-                isGamePaused = true;
-                canvas2.SetActive(true);
-                PauseGame();
-              
                 
-                
-            }
-            else
-            {
+
                 isGamePaused = false;
                 canvas2.SetActive(false);
                 ResumeGame();
-                
+
+            }
+            else
+            {
+                isGamePaused = true;
+                canvas2.SetActive(true);
+                PauseGame();
+
             }
         }
     }
-    void LoadScene()
-    {
-        SceneManager.LoadScene(sceneToLoad);
-    }
+    
     public void ResumeGame()
     {
         Time.timeScale = 0f; // Set time scale to normal (unpause)
         isGamePaused = false;
-
+        canvas2.SetActive(false);
     }
 
     public void PauseGame()
     {
         Time.timeScale = 1f; // Set time scale to 0 (pause)
         isGamePaused = true;
-
+        
     }
 
     public void QuitGame()
@@ -74,7 +68,7 @@ public class GameManager : MonoBehaviour
     public void LoadMainMenu()
     {
         // This function should be called when the player wants to go back to the main menu
-         // Ensure time scale is set to normal before loading main menu
+        // Ensure time scale is set to normal before loading main menu
         SceneManager.LoadScene("Scene"); // Replace "MainMenu" with the name of your main menu scene
 
     }
