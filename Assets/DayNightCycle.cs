6b321e15f0d9071699cb3b09f27256794b536cb6
Diamond26400 <obaloluwajames5@gmail.com>
2023-12-28 15:52:37 +0100

    Update

---
+++ Assets/DayNightCycle.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [SerializeField] private Light sun;
+    [SerializeField] private Material skyboxMaterial;
+    [SerializeField] private float dayDuration = 1200f; // in seconds
+
+    private void Update()
+    {
+        float timeOfDay = Mathf.Repeat(Time.time / dayDuration, 1f);
+        UpdateSun(timeOfDay);
+        UpdateSkybox(timeOfDay);
+    }
+
+    private void UpdateSun(float timeOfDay)
+    {
+        sun.transform.localRotation = Quaternion.Euler(timeOfDay * 360f, 45f, 0f);
+    }
+
+    private void UpdateSkybox(float timeOfDay)
+    {
+        // Adjust skybox properties based on time of day
+        Color skyColor = Color.Lerp(Color.black, Color.blue, timeOfDay);
+        skyboxMaterial.SetColor("_SkyTint", skyColor);
+    }
+}
