1cc653e7ec2357e5e4f35fdb40601d72bbff4b16
Diamond26400 <obaloluwajames5@gmail.com>
2023-12-30 12:53:08 +0100

    Update

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+[CreateAssetMenu (fileName = "New Item", menuName = "Item/Create New Item")]
 
-public class Item : MonoBehaviour
+public class Item : ScriptableObject
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int id;
+    public string itemName;
+    public int value;
+    public Sprite icon;
 
     // Update is called once per frame
     void Update()
