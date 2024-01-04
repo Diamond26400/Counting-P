216952eb4de517bd35f01c7f5f69397426958b18
Diamond26400 <obaloluwajames5@gmail.com>
2024-01-04 15:44:09 +0100

    uPDATE

--- Assets/Script/InventoryManager.cs
+++ Assets/Script/InventoryManager.cs
@@ -9,11 +9,13 @@ public class InventoryManager : MonoBehaviour
     public GameObject itemPrefab; // Reference to the item prefab
     public Transform inventoryPanel; // Reference to the inventory panel
 
+    private GameObject selectedPrefab; // The currently selected item prefab
 
     // Start is called before the first frame update
     void Awake()
     {
         Instance = this;
+        selectedPrefab = itemPrefab; // For simplicity, assume the same prefab is selected every tim
     }
 
     // Update is called once per frame
@@ -29,7 +31,18 @@ public class InventoryManager : MonoBehaviour
     {
         Instantiate(itemPrefab, inventoryPanel);
     }
-
+    public void SpawnItem(Vector3 spawnPoint)
+    {
+        if (selectedPrefab != null)
+        {
+            Instantiate(selectedPrefab, spawnPoint, Quaternion.identity);
+            Debug.Log("Item spawned at: " + spawnPoint);
+        }
+        else
+        {
+            Debug.LogWarning("No item selected.");
+        }
+    }
 }
 
 
