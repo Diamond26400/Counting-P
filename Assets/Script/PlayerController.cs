e231b9cf8547333e8037f23437a89aa315361552
Diamond26400 <obaloluwajames5@gmail.com>
2024-01-04 23:26:11 +0100

    Update PlayerController.cs

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -11,13 +11,16 @@ public class PlayerController : MonoBehaviour
 
     public float horizontalcomponent;
     public float verticalComponent;
-   [SerializeField] float speed = 5.0f;
+    [SerializeField] float speed = 5.0f;
     [SerializeField] float rotationSpeed = 5.0f;
+    [SerializeField] float maxLookUp = 80f; // Maximum allowed look up angle
+    [SerializeField] float maxLookDown = 80f; // Maximum allowed look down angle
+
     private void Start()
     {
-        
-        Update();
+        // Remove Update() call from Start() as Update is automatically called each frame.
     }
+
     public void Update()
     {
         horizontalcomponent = Input.GetAxis("Horizontal");
@@ -27,27 +30,37 @@ public class PlayerController : MonoBehaviour
         movement.Normalize();
         transform.Translate(movement * speed * Time.deltaTime);
 
+        // Rotate up (pitch) when W is held down
+        if (Input.GetKey(KeyCode.W))
+        {
+            RotateCamera(-rotationSpeed);
+        }
+
+        // Rotate down (pitch) when S is held down
+        if (Input.GetKey(KeyCode.S))
+        {
+            RotateCamera(rotationSpeed);
+        }
+
         // Get mouse input for rotation
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
+        // float mouseX = Input.GetAxis("Mouse X");
+        // float mouseY = Input.GetAxis("Mouse Y");
 
         // Rotate the camera vertically (pitch)
-        transform.Rotate(Vector3.left * mouseY * rotationSpeed);
+        // RotateCamera(mouseY * rotationSpeed);
 
         // Rotate the entire player horizontally (yaw)
-        transform.Rotate(Vector3.up * mouseX * rotationSpeed);
-
-        if (transform.rotation.eulerAngles.x + mouseY < 90f || transform.rotation.eulerAngles.x + mouseY > 270f)
-        {
-            transform.Rotate(Vector3.left * mouseY * rotationSpeed);
-        }
-
-        // Rotate the entire player horizontally (yaw)
-        if (transform.parent != null)
-        {
-            transform.parent.Rotate(Vector3.up * mouseX * rotationSpeed);
-        }
+        // transform.Rotate(Vector3.up * mouseX * rotationSpeed);
     }
+
+    private void RotateCamera(float amount)
+    {
+        // Rotate the camera's local rotation around the x-axis (pitch)
+        float newRotationX = playerCamera.transform.localRotation.eulerAngles.x + amount * Time.deltaTime;
+        newRotationX = Mathf.Clamp(newRotationX, -maxLookUp, maxLookDown);
+        playerCamera.transform.localRotation = Quaternion.Euler(newRotationX, 0f, 0f);
+    }
+
     public Vector3 GetSpawnPoint()
     {
         // Calculate the spawn point in front of the camera
@@ -56,9 +69,7 @@ public class PlayerController : MonoBehaviour
         // Adjust the spawn point to be at the same height as the camera
         spawnPoint.y = playerCamera.transform.position.y;
 
-
-    
         return spawnPoint;
     }
-    
 }
+
