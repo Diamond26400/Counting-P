14c8ae14a04192624fdd159f4198c2e1e828403d
Diamond26400 <obaloluwajames5@gmail.com>
2024-01-04 16:54:05 +0100

    Update PlayerController.cs

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,8 +9,34 @@ public class PlayerController : MonoBehaviour
     // Adjust the offset to determine the spawn position in front of the camera
     public float spawnDistance = 2f;
 
-    // ... other player controller code
+    public float horizontalcomponent;
+    public float verticalComponent;
+   [SerializeField] float speed = 5.0f;
+    [SerializeField] float rotationSpeed = 5.0f;
+    private void Start()
+    {
+        
+        Update();
+    }
+    public void Update()
+    {
+        horizontalcomponent = Input.GetAxis("Horizontal");
+        verticalComponent = Input.GetAxis("Vertical");
 
+        Vector3 movement = new Vector3(horizontalcomponent, 0f, verticalComponent);
+        movement.Normalize();
+        transform.Translate(movement * speed * Time.deltaTime);
+
+        // Get mouse input for rotation
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        // Rotate the camera vertically (pitch)
+        transform.Rotate(Vector3.left * mouseY * rotationSpeed);
+
+        // Rotate the entire player horizontally (yaw)
+        transform.parent.Rotate(Vector3.up * mouseX * rotationSpeed);
+    }
     public Vector3 GetSpawnPoint()
     {
         // Calculate the spawn point in front of the camera
@@ -19,6 +45,9 @@ public class PlayerController : MonoBehaviour
         // Adjust the spawn point to be at the same height as the camera
         spawnPoint.y = playerCamera.transform.position.y;
 
+
+    
         return spawnPoint;
     }
+    
 }
