e6659e36de00a2f5335a0004198f50e270576219
Diamond26400 <obaloluwajames5@gmail.com>
2024-01-28 23:03:19 +0100

    Update PlayerController.cs

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -14,34 +14,17 @@ public class PlayerController : MonoBehaviour
     {
         // Get the horizontal and vertical input axes
         float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+      
 
         // Calculate the rotation angles
         float yawAngle = horizontalInput * rotationSpeed * Time.deltaTime;
-        float pitchAngle = verticalInput * rotationSpeed * Time.deltaTime;
+        
 
-        // Apply rotation to the camera's transform based on keys
-        if (Input.GetKey(KeyCode.D))
-        {
-            playerCamera.transform.Rotate(Vector3.up, -yawAngle, Space.Self);  // Yaw rotation left
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            playerCamera.transform.Rotate(Vector3.down, yawAngle, Space.Self);   // Yaw rotation right
-        }
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            playerCamera.transform.Rotate(Vector3.left, pitchAngle, Space.Self);  // Pitch rotation up
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            playerCamera.transform.Rotate(Vector3.right, -pitchAngle, Space.Self); // Pitch rotation down
-
+     
         }
 
 
     }
 
     
-}
+
