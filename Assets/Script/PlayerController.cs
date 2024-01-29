f626184eacfa01c2788964c358a8cc3ecabd005f
Diamond26400 <obaloluwajames5@gmail.com>
2024-01-29 21:46:38 +0100

    Update PlayerController.cs

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,16 +8,18 @@ public class PlayerController : MonoBehaviour
 {
     public Camera playerCamera; // Reference to the player's camera
     
-    [SerializeField] float rotationSpeed = 5.0f;
+    [SerializeField] float speed = 5.0f;
 
     void Update()
     {
         // Get the horizontal and vertical input axes
         float horizontalInput = Input.GetAxis("Horizontal");
       
-
-        // Calculate the rotation angles
-        float yawAngle = horizontalInput * rotationSpeed * Time.deltaTime;
+  // Calculate movement vector
+        Vector3 movement = new Vector3(horizontalInput, 0f, 0f);
+        
+         // Move the camera
+        transform.Translate(movement * speed * Time.deltaTime);
         
 
      
